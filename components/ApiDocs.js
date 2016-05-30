19aba1ef021ef5f1e741fa030ac45ffcef886251
sklirg <hlsolbjorg@gmail.com>
2016-05-30 17:43:59 +0200

    Add a trailing slash to the Swagger client path

--- components/ApiDocs.js
+++ components/ApiDocs.js
@@ -4,7 +4,7 @@ import { ProgressBar, Tabs, Tab } from 'react-mdl'
 const ApiDocs = ({selectedApi, thisApi}) => (
   <div>
     <p>API documentation follows the standard set by Swagger and the Open API Initiative. 
-    Microserv hosts its own Swagger client available at <a href="https://despina.128.no/swagger" target="_blank">https://despina.128.no/swagger</a>.
+    Microserv hosts its own Swagger client available at <a href="https://despina.128.no/swagger/" target="_blank">https://despina.128.no/swagger/</a>.
     </p>
     { (selectedApi !== "frontend" ? <p>
       Provide the Swagger client with this path to read API documentation for {selectedApi}: <a href={thisApi.apiUri + "/static/swagger.json"} target="_blank">{thisApi.apiUri + "/static/swagger.json"}</a>
