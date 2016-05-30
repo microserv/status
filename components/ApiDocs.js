293a32b5ae76fda1489873d8156785c6d1a63b98
sklirg <hlsolbjorg@gmail.com>
2016-05-30 20:15:29 +0200

    Also say that microauth doesnt offer an API

--- components/ApiDocs.js
+++ components/ApiDocs.js
@@ -6,9 +6,9 @@ const ApiDocs = ({selectedApi, swaggerClient, swaggerDocs, thisApi}) => (
     <p>API documentation follows the standard set by <a href="http://swagger.io" target="_blank">Swagger</a> and the Open API Initiative. 
     Microserv hosts its own Swagger client available at <a href="https://despina.128.no/swagger/" target="_blank">https://despina.128.no/swagger/</a>.
     </p>
-    { (selectedApi !== "frontend" ? <p>
+    { (selectedApi !== "frontend" && selectedApi !== "microauth"? <p>
       Click <a href={swaggerClient + "?url=" + swaggerDocs} target="_blank">here</a> or provide the Swagger client with this path to read API documentation for {selectedApi}: <a href={thisApi.apiUri + swaggerDocs} target="_blank">{thisApi.apiUri + "/static/swagger.json"}</a>
-    </p> : <p>The frontend service does not offer an API.</p> )}
+    </p> : <p>The {selectedApi} service does not offer an API.</p> )}
   </div>
 )
 
