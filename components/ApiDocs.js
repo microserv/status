e76b8d20c54ab8c68cff7a284ba2316eba648526
sklirg <hlsolbjorg@gmail.com>
2016-05-30 17:41:22 +0200

    Add links to API documentation

--- components/ApiDocs.js
+++ components/ApiDocs.js
@@ -1,9 +1,14 @@
 import React, { PropTypes } from 'react'
 import { ProgressBar, Tabs, Tab } from 'react-mdl'
 
-const ApiDocs = () => (
+const ApiDocs = ({selectedApi, thisApi}) => (
   <div>
-    <p>Content</p>
+    <p>API documentation follows the standard set by Swagger and the Open API Initiative. 
+    Microserv hosts its own Swagger client available at <a href="https://despina.128.no/swagger" target="_blank">https://despina.128.no/swagger</a>.
+    </p>
+    { (selectedApi !== "frontend" ? <p>
+      Provide the Swagger client with this path to read API documentation for {selectedApi}: <a href={thisApi.apiUri + "/static/swagger.json"} target="_blank">{thisApi.apiUri + "/static/swagger.json"}</a>
+    </p> : <p>The frontend service does not offer an API.</p> )}
   </div>
 )
 
--- containers/ApiDocs.js
+++ containers/ApiDocs.js
@@ -3,10 +3,18 @@ import { fetchDocs, selectApi } from '../actions'
 import ApiDocs from '../components/ApiDocs'
 
 const mapStateToProps = (state, ownProps) => {
+  let currentState = state
+  let thisApi = null
+  for (let i = 0; i < currentState.apis.length; i++) {
+    if (currentState.apis[i].name === currentState.selectedApi) {
+      thisApi = currentState.apis[i]
+    }
+  }
   return {
     apis: state.apis,
     docsByApi: state.docsByApi,
-    selectedApi: state.selectedApi
+    selectedApi: state.selectedApi,
+    thisApi: thisApi
   }
 }
 
